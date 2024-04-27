f4d2bc638425879711a0eb020c812ac8f4c36832
Lowiq <56718711+GetLoloed@users.noreply.github.com>
2024-04-27 12:27:32 +0200

    Add Swagger documentation to routes
    Detailed Swagger documentation has been added to "userRoutes.js" and "statsRoutes.js". This includes expected responses and required fields for various API endpoints such as "/register", "/login", "/getUserID", and "/stats". Additionally, descriptions, summaries, and tags have been provided for better understandability.

--- backend/routes/statsRoutes.js
+++ backend/routes/statsRoutes.js
@@ -2,6 +2,31 @@ const express = require('express');
 const router = express.Router();
 const statsController = require('../controllers/statsController');
 
+/**
+ * @swagger
+ * /stats:
+ *   get:
+ *     tags:
+ *       - Stats
+ *     summary: Get statistics
+ *     description: Retrieves statistics from the database
+ *     responses:
+ *       200:
+ *         description: Statistics successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   Catégorie:
+ *                     type: string
+ *                   Nombre:
+ *                     type: integer
+ *       500:
+ *         description: Error occurred while retrieving the statistics
+ */
 router.get('/stats', statsController.getStats);
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,91 @@ const express = require('express');
 const userController = require("../controllers/userController");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Register a new user
+ *     description: Registers a new user in the database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User successfully registered
+ *       500:
+ *         description: Error occurred while registering the user
+ */
 router.post("/register", userController.register);
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Login a user
+ *     description: Logs in a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User successfully logged in
+ *       403:
+ *         description: Incorrect username or password
+ *       500:
+ *         description: Error occurred while logging in
+ */
 router.post("/login", userController.login);
+
+/**
+ * @swagger
+ * /getUserID:
+ *   get:
+ *     tags:
+ *       - Users
+ *     summary: Get user ID
+ *     description: Retrieves the ID of a user
+ *     parameters:
+ *       - in: query
+ *         name: username
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: User ID successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user_id:
+ *                   type: integer
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Error occurred while retrieving the user ID
+ */
 router.get('/getUserID', userController.getUserID);
 
 module.exports = router;
